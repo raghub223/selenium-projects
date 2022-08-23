595e04f2b220b00af1a7155e0862ee7a1a3b5677
raghub223 <braghu0422@gmail.com>
2022-08-23 13:40:08 +0530

    first commit

---
+++ src/test/Testvariables.java
@@ -0,0 +1,27 @@
+package test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Testvariables {
+	public static void main(String[] args) throws IOException
+	{
+		Properties prop = new Properties();
+		FileInputStream fis = new FileInputStream("C:\\Users\\ragha\\Study\\TestAutomation\\GlobalEnvVariables\\src\\data.properties");
+		prop.load(fis);
+		
+		System.out.println(prop.getProperty("browser"));
+		System.out.println(prop.getProperty("url"));
+		
+		prop.setProperty("browser", "firefox");
+		System.out.println(prop.getProperty("browser"));
+		FileOutputStream fos = new FileOutputStream("C:\\Users\\ragha\\Study\\TestAutomation\\GlobalEnvVariables\\src\\data.properties");
+		
+		prop.store(fos, null);
+		
+		
+	}
+}
